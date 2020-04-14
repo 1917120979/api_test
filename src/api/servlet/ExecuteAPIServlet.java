eda871f752fe11761b25490ec085aa6b145ed99f
1917120979 <54828660+1917120979@users.noreply.github.com>
2020-04-14 17:51:14 +0800

    0414-2

---
+++ src/api/servlet/ExecuteAPIServlet.java
@@ -0,0 +1,41 @@
+package api.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import api.util.Page;
+
+@SuppressWarnings("serial")
+public class ExecuteAPIServlet extends BaseBackServlet{
+
+	@Override
+	public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+		int aid;
+		return null;
+	}
+
+	@Override
+	public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
